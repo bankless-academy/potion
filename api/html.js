20ee2bb0a490fe6ec111fc8951a50810ad9b22a5
Didier Krux <didier@krux.co>
2023-05-27 12:04:05 +0200

    handle issue with sublists

--- api/html.js
+++ api/html.js
@@ -18,10 +18,10 @@ const textArrayToHtml = require("../notion/textArrayToHtml.js")
 const WHITELIST = require("../helpers/whitelist")
 
 module.exports = async (req, res) => {
-  const { id:queryId } = req.query
+  const { id: queryId } = req.query
   const id = normalizeId(queryId)
 
-  if(!id) {
+  if (!id) {
     return res.json({
       error: "no Notion doc ID provided as `id` parameter"
     })
@@ -42,7 +42,7 @@ module.exports = async (req, res) => {
   if (!parent_id || !WHITELIST.includes(parent_id))
     return res.json({ error: `forbidden, add ${parent_id} to WHITELIST first` })
 
-  if(!overview.recordMap?.block[id].value) {
+  if (!overview.recordMap?.block[id].value) {
     return res.json({
       error: "could not read Notion doc with this ID - make sure public access is enabled"
     })
@@ -50,7 +50,7 @@ module.exports = async (req, res) => {
 
   const contentIds = overview.recordMap.block[id].value.content
 
-  if(!contentIds) {
+  if (!contentIds) {
     // BA custom: return page title if page content is empty
     const [[title]] = overview.recordMap.block[id].value?.properties?.title
     // console.log('title:', title)
@@ -67,27 +67,27 @@ module.exports = async (req, res) => {
   let lastChunk
   let hasMorePageChunks = true
 
-  while(hasMorePageChunks) {
+  while (hasMorePageChunks) {
     const cursor = lastChunk && lastChunk.cursor || ({ stack: [] })
 
     const chunk = await call("loadPageChunk", {
-        pageId: id,
-        limit: 100,
-        cursor,
-        chunkNumber: 0,
-        verticalColumns: false
+      pageId: id,
+      limit: 100,
+      cursor,
+      chunkNumber: 0,
+      verticalColumns: false
     })
 
     recordMap = { ...recordMap, ...chunk.recordMap.block }
 
     lastChunk = chunk
 
-    if(chunk.cursor.stack.length === 0) hasMorePageChunks = false
+    if (chunk.cursor.stack.length === 0) hasMorePageChunks = false
   }
 
   contentIds.forEach(id => {
     const block = recordMap[id]
-    if(block) contents.push(block.value)
+    if (block) contents.push(block.value)
   })
 
   const html = []
@@ -96,7 +96,7 @@ module.exports = async (req, res) => {
     const type = block.type
     // console.log('block', block)
 
-    if(["header", "sub_header", "sub_sub_header", "text"].includes(type)) {
+    if (["header", "sub_header", "sub_sub_header", "text"].includes(type)) {
       /* Headers (H1 - H3) and plain text */
       const el = {
         header: "h1",
@@ -105,14 +105,14 @@ module.exports = async (req, res) => {
         text: "p"
       }[type]
 
-      if(!block.properties) {
+      if (!block.properties) {
         // This is an empty text block.
         return
       }
       // console.log(block)
-      const notionId=["header"].includes(type) ? ` notion-id="${block.id.replace(/-/g, '')}"` : '';
+      const notionId = ["header"].includes(type) ? ` notion-id="${block.id.replace(/-/g, '')}"` : '';
       html.push(`<${el}${notionId}>${textArrayToHtml(block.properties?.title)}</${el}>`)
-    } else if(["numbered_list", "bulleted_list"].includes(type)) {
+    } else if (["numbered_list", "bulleted_list"].includes(type)) {
       /* Numbered and bulleted lists */
       const el = {
         "numbered_list": "ol",
@@ -123,31 +123,31 @@ module.exports = async (req, res) => {
       let subList = ''
       if (block.content) {
         // HACK: add space to avoid replace hack (bottom of the file)
-        subList+=` <ul>`
+        subList += ` <ul>`
         for (const subListID of block.content) {
-          const subBlock = recordMap[subListID].value
+          const subBlock = recordMap[subListID]?.value
           // console.log('subBlock', subBlock)
-          if (["numbered_list", "bulleted_list"].includes(subBlock.type)) {
+          if (subBlock && ["numbered_list", "bulleted_list"].includes(subBlock.type)) {
             // console.log('v', subBlock.properties?.title)
-            subList+=`<li>${textArrayToHtml(subBlock.properties && subBlock.properties?.title)}</li>`
+            subList += `<li>${textArrayToHtml(subBlock.properties && subBlock.properties?.title)}</li>`
           }
         }
-        subList+=`</ul> `
+        subList += `</ul> `
       }
 
       html.push(`<${el}><li>${textArrayToHtml(block.properties && block.properties?.title)}</li>${subList}</${el}>`)
-    } else if(["to_do"].includes(type)) {
+    } else if (["to_do"].includes(type)) {
       /* To do list represented by a list of checkbox inputs */
-      const checked = Boolean(block.properties?.checked?.toString()==="Yes")
+      const checked = Boolean(block.properties?.checked?.toString() === "Yes")
       html.push(`<div class="checklist"><label><input type="checkbox" disabled${checked ? " checked" : ""}>${textArrayToHtml(block.properties?.title)}</label></div>`)
-    } else if(["code"].includes(type)) {
+    } else if (["code"].includes(type)) {
       /* Full code blocks with language */
       const language = block.properties?.language[0][0].toLowerCase().replace(/ /g, "")
       const text = block.properties?.title || [[""]]
 
       // Inject unescaped HTML if code block's language is set to LiveScript
       const showLive = language === "livescript"
-      if(showLive) {
+      if (showLive) {
         html.push(text.map(clip => clip[0]).join("")) // Ignore styling, just take the text
       } else {
         const code = textArrayToHtml(text, { br: false, escape: false })
@@ -155,10 +155,10 @@ module.exports = async (req, res) => {
         try {
           // try/catch because this fails when prism doesn't know the language
           highlighted = prism.highlight(code, prism.languages[language], language)
-        } catch{}
+        } catch { }
         html.push(`<pre><code class="language-${language}">${highlighted}</code></pre>`)
       }
-    } else if(["callout"].includes(type)) {
+    } else if (["callout"].includes(type)) {
       /* Callout formatted with emoji from emojicdn.elk.sh or just image */
       const icon = block.format.page_icon
       const imageLink = icon.startsWith("http") ? `https://www.notion.so/image/${encodeURIComponent(icon)}?table=block&id=${block.id}` : `https://emojicdn.elk.sh/${icon}`
@@ -167,25 +167,25 @@ module.exports = async (req, res) => {
       const text = block.properties?.title
       // custom: don't export callout
       // html.push(`<div class="callout${isBackground ? " background" : " color"}-${color}"><img src="${imageLink}"><p>${textArrayToHtml(text)}</p></div>`)
-    } else if(["quote"].includes(type)) {
+    } else if (["quote"].includes(type)) {
       html.push(`<blockquote>${textArrayToHtml(block.properties?.title)}</blockquote>`)
-    } else if(["divider"].includes(type)) {
+    } else if (["divider"].includes(type)) {
       html.push(`<hr>`)
-    } else if(["image"].includes(type)) {
+    } else if (["image"].includes(type)) {
       // console.log('img', block)
       html.push(`<img src="https://www.notion.so/image/${encodeURIComponent(block.format.display_source)}?table=block&id=${block.id}">`)
-    } else if(["equation"].includes(type)) {
-      if(!block.properties) {
+    } else if (["equation"].includes(type)) {
+      if (!block.properties) {
         // Equation block is empty
         return
       }
       const equation = block.properties?.title[0][0]
       const equationHtml = katex.renderToString(equation, { throwOnError: false })
       html.push(`<div class="equation">${equationHtml}</div>`)
-    } else if(["embed"].includes(type)) {
+    } else if (["embed"].includes(type)) {
       // console.log('embed', block)
       html.push(`<iframe src="${block.properties?.source[0][0]}"></iframe>`)
-    } else if(["video"].includes(type)) {
+    } else if (["video"].includes(type)) {
       // video + lottie animations
       // console.log('video', block)
       html.push(`<iframe src="${block.format.display_source}"></iframe>`)
@@ -196,14 +196,14 @@ module.exports = async (req, res) => {
   })
 
   // Only add Katex stylesheet if there's Katex elements.
-  if(html.join("").includes(`class="katex"`)) {
+  if (html.join("").includes(`class="katex"`)) {
     html.push(`<link rel="stylesheet" href="https://unpkg.com/katex@0.12.0/dist/katex.min.css">`)
   }
 
   const joinedHtml = html.join("")
   const cleanedHtml = joinedHtml
-                        .replace(/<\/ol><ol>/g, "")
-                        .replace(/<\/ul><ul>/g, "")
-                        .replace(/<\/div><div class="checklist">/g, "")
+    .replace(/<\/ol><ol>/g, "")
+    .replace(/<\/ul><ul>/g, "")
+    .replace(/<\/div><div class="checklist">/g, "")
   res.send(cleanedHtml)
 }
